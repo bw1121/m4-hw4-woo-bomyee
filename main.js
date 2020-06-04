8ec9823e95d56cb16e082ee0e57ee666dd29b0b1
Bomyee Woo <bomyeew@gmail.com>
2020-06-04 13:52:12 -0400

    Add and invoke function orderPizza

--- main.js
+++ main.js
@@ -1,8 +1,17 @@
 var pizza = {
     crust: "thin",
-    cheese: "mozarella",
+    cheese: "mozzarella",
     sauce: "tomato",
     addToppings: function() {
         console.log("Please add pepperoni and sausage");
     }
 };
+
+function orderPizza() {
+    console.log(pizza.crust);
+    console.log(pizza.cheese);
+    console.log(pizza.sauce);
+    console.log(pizza.addToppings());
+}
+
+orderPizza();
